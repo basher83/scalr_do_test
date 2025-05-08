2c28b9915744e1d8f8f27c56abad17a5cc28e483
basher83 <129473091+basher83@users.noreply.github.com>
2025-05-08 21:44:31 +0000

    Remove ansible_inventory output from Terraform configuration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,3 @@ output "droplet_ip" {
   value       = digitalocean_droplet.drop_test.ipv4_address
   description = "The public IPv4 address of the droplet"
 }
-
-output "ansible_inventory" {
-  value = templatefile("${path.module}/../ansible/inventory/inventory_template.yml", {
-    droplet_ip   = digitalocean_droplet.drop_test.ipv4_address
-    droplet_name = var.droplet_name
-  })
-}
